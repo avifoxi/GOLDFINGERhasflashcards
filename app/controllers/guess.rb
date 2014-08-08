8b1062ec1d8e3a000f618e93bc14ce34480b1a55
AviFoxi <frosenox@gmail.com>
2014-08-08 16:36:57 -0400

    merge in changes

--- app/controllers/guess.rb
+++ app/controllers/guess.rb
@@ -1,4 +1,4 @@
 post 'round/:round_id/card/:card_id/guess/create' do
-    puts "@guess = Guess.create(params)"
-    puts "redirect to round/:round_id"
+  puts "@guess = Guess.create(params)"
+  puts "redirect to round/:round_id"
 end
