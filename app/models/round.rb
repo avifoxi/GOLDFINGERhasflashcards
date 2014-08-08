17ebd4bfab3e448c6db062bbe376c11d35fe939b
Joseph White <josephbarrywhite@gmail.com>
2014-08-08 18:12:06 -0400

    add over? and next_card methods to round.rb

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,7 +1,22 @@
-class Round < ActiveRecord::Base
+  class Round < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :user
   belongs_to :deck
   has_many :guesses
   has_many :cards, through: :guesses
+
+  def over?
+    @played_cards ||= 0
+    return true if @played_cards == self.deck.cards.length
+    @played_cards += 1
+  end
+
+
+  def next_card
+    self.deck.cards.sample
+  end
+
+end
+
+
 end
