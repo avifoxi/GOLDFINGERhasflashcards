cab235af236ee96611c40426756250d770c8d38b
Eddie Kim & Joseph White <pair+EddieKim.Joseph-White@devbootcamp.com>
2014-08-08 17:50:32 -0400

    create users and 2 decks for seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,52 @@
 ###  SIMPLE USER TEST ROUND CREATION
 
-fishie = User.create(name: 'fishie')
+eddie = User.create(name: "Eddie", email: "ehk@gmail.com", password: "123" )
+avi = User.create(name: 'Avi', email: "avi@avi.avi", password: "avi")
 
-deck1 = Deck.create(name: 'fish qs')
+math_deck = Deck.create(name: 'Math')
+animal_deck = Deck.create(name: 'Animals')
 
-round1 = fishie.rounds.create
+eddie_round_1 = eddie.rounds.create
 #round1 = fishie.rounds.create(deck: deck1) -- in place of line10 when paired with line 7
+# eddie_round_1.rounds << round1
+eddie_round_1.decks << animal_deck
 
-deck1.rounds << round1
-# round1.decks << deck1
+5.times do |i|
+  math_deck.cards << Card.create(question: 'what is this number?', answer: i.to_s)
+end
 
-card1 = Card.create(question: 'are you a fish', answer: 'yes')
+animals_array = %w(Cheetah Tiger Lion Panther Jaguar)
 
-card2 = Card.create(question: 'are you a dog', answer: 'no')
+animals_array.each do |row|
+  animal_deck.cards << Card.create(question: 'what animal is this?', answer: row )
+end
+# math_card1 = Card.create(question: 'are you a fish', answer: 'yes')
+# math_card2 = Card.create(question: 'are you a dog', answer: 'no')
+# math_card3 = Card.create(question: 'are you a fish', answer: 'yes')
+# math_card4 = Card.create(question: 'are you a dog', answer: 'no')
+# math_card5 = Card.create(question: 'are you a fish', answer: 'yes')
 
-deck1.cards << card1
-deck1.cards << card2
+# card = Card.create(question: 'are you a dog', answer: 'no')
+# card1 = Card.create(question: 'are you a fish', answer: 'yes')
+# card2 = Card.create(question: 'are you a dog', answer: 'no')
+# card1 = Card.create(question: 'are you a fish', answer: 'yes')
+# card2 = Card.create(question: 'are you a dog', answer: 'no')
 
-round1.cards # => 2
+
+# deck1.cards << card1
+# deck1.cards << card2
+
+# round1.cards # => 2
 
 
-deck1.cards # => card1
+# deck1.cards # => card1
 
-card1.guesses.create(user_input: 'no')
-card1.guesses.create(user_input: 'yes')
+# card1.guesses.create(user_input: 'no')
+# card1.guesses.create(user_input: 'yes')
 
-c1guesses = card1.guesses # =>
+# c1guesses = card1.guesses # =>
 
-fishie.rounds # =>
+# fishie.rounds # =>
 
 
 
