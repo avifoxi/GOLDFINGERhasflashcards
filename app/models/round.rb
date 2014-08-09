1cfa3e562fc568c40124ab7df1002fa0d6c314af
Joseph White <josephbarrywhite@gmail.com>
2014-08-08 20:49:09 -0400

    create logic for round model

--- app/models/round.rb
+++ app/models/round.rb
@@ -6,11 +6,20 @@ class Round < ActiveRecord::Base
   has_many :cards, through: :guesses
 
   def over?
-    @played_cards ||= 0
-    return true if @played_cards == self.deck.cards.length
-    @played_cards += 1
+    @card_list ||= self.deck.cards.map { |card| card.id }
+    return true if @card_list.empty?
+    @next_card = Card. findby id = card_list.sample
+    @card_list.delete(@next_card)
+
+    # true unless @played_cards
   end
 
+# cards = self.deck.cards
+# when guess is made, remove card with card_id from cards
+
+  def played_card
+    Card.find(id: Guess.last.card_id)
+  end
 
   def next_card
     self.deck.cards.sample
