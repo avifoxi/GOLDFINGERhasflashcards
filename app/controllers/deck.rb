64996dc809e096b029768f0b7b73a8eb1ecd66ef
Joseph White <josephbarrywhite@gmail.com>
2014-08-08 22:12:43 -0400

    update deck.rb and show_all.erb to have user choose deck and start round, update play.erb to show first card

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,7 +1,13 @@
 get '/decks' do
-  puts "display list of decks, let user pick one"
+  @decks = Deck.all
+  erb :"/decks/show_all"
 end
 
 post '/decks/:deck_id/round/create' do
-  puts "@round = Round.create(user, deck)"
+  # @user = User.find(session[:id]) if session[:id]
+  @user = User.find(1) # replace with line above once login is implemented
+  @deck = Deck.find(params[:deck_id])
+  @round = Round.create(user: @user, deck: @deck)
+  @card = @round.next_card
+  erb :"/decks/rounds/play"
 end
