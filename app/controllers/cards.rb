df6f54421d1387808d6fd6163c57a9fd867dc1bf
AviFoxi <frosenox@gmail.com>
2014-08-09 19:39:19 -0400

    add patch route for card creation

--- app/controllers/cards.rb
+++ app/controllers/cards.rb
@@ -11,13 +11,34 @@ post "/decks/:deck_id/cards/create" do
 		@deck.cards << @card
 		redirect "/decks/#{@deck.id}"
 	else
+		@card.id = 0
 		session[:error] = 'Card creation Error'
 		erb :"decks/cards/edit"
 	end
 end
 
-get '/decks/:deck_id/card/:card_id/edit' do
+get '/decks/:deck_id/cards/:card_id/edit' do
  	@card = Card.find(params[:card_id])
 	@deck = Deck.find(params[:deck_id])
 	erb :"decks/cards/edit"
+end
+
+patch '/decks/:deck_id/cards/:card_id/edit' do
+	@deck = Deck.find(params[:deck_id])
+
+ 	## if card has ID of 0 - ie unsaved new with errors
+ 	
+ 	if params[:card_id] == 0
+ 		@card = Card.new(params[:card])
+ 	else
+ 		@card = Card.find(params[:card_id]) 
+ 	end
+ 
+ 	if @card.update_attributes(params[:card])
+ 		@deck.cards << @card
+ 		redirect "/decks/#{@deck.id}"
+ 	else 
+ 		erb :"decks/cards/edit"
+ 	end
+
 end
