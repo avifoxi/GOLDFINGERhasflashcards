a5909bd3ce7b3e565b6f4cf6d8a114b88ea51f30
AviFoxi <frosenox@gmail.com>
2014-08-09 18:52:18 -0400

    finish post route for card creation, redirect to error page if card creation error on save

--- app/controllers/cards.rb
+++ app/controllers/cards.rb
@@ -5,11 +5,21 @@ get "/decks/:deck_id/card/new" do
 end
 
 post "/decks/:deck_id/card/create" do
+	@card = Card.new(params[:card])
+	@deck = Deck.find(params[:deck_id])
+	if @card.save
+		@deck.cards << @card
+		redirect "/decks/#{@deck.id}"
+	else
+		session[:error] = 'Card creation Error'
+		erb :"decks/cards/edit"
+	end
+
  
-  erb :"decks/cards/new"
+  # erb :"decks/cards/new"
 end
 
-get '/decks/:deck_id/card/edit' do 
+get '/decks/:deck_id/card/:card_id/edit' do 
 
-	'i am a card. update me please'
+	erb :"decks/cards/edit"
 end
