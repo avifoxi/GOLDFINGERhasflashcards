52ddf26fb4541607320aaeb392a8d2fb67f9ada9
Joseph White <josephbarrywhite@gmail.com>
2014-08-09 01:18:40 -0400

    update to generate Guesses, have issue with needing to create Guess before user input

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -9,24 +9,6 @@ post '/decks/:deck_id/round/create' do
   @deck = Deck.find(params[:id])
   @round = Round.create(user: @user, deck: @deck)
   session[:round_id] = @round.id
-  # @card = @round.next_card
-  # erb :'/decks/rounds/play'
   redirect '/decks/rounds/play'
 end
-
-# get '/decks/rounds/play' do
-#   # puts <<-TEXT
-#   #   display the page youre going to play on
-#   #   @round = Round.find(:id)
-#   #   if @round.over? <-(we'll have to make this method)
-#   #     redirect to users restuls
-#   #   else
-#   #     @card = @round.next_card <-(we'll have to make this method)
-#   #     redirect to 'round/:id'
-#   #  TEXT
-#   puts "hi there"
-
-
-#   erb :'/decks/rounds/play'
-# end
 
--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -1,21 +1,25 @@
 get '/decks/rounds/play' do
+
+  @round = Round.find(session[:round_id])
+  @card = @round.next_card
+  @guess = Guess.create(round_id: @round.id, card_id: @card.id, user_input: "need input")
+  session[:guess_id] = @guess.id
+
+  erb :'/decks/rounds/play'
+end
+
+get '/decks/rounds/results' do
+
+  @round = Round.find(session[:round_id])
+  erb :'/decks/rounds/results'
+end
+
   # puts <<-TEXT
   #   display the page youre going to play on
   #   @round = Round.find(:id)
   #   if @round.over? <-(we'll have to make this method)
   #     redirect to users restuls
   #   else
-  #   	@card = @round.next_card <-(we'll have to make this method)
+  #     @card = @round.next_card <-(we'll have to make this method)
   #     redirect to 'round/:id'
   #  TEXT
-  puts "hi"
-  p params
-  @round = Round.find(session[:round_id])
-  @user = @round.user
-  @deck = @round.deck
-  @card = @round.next_card
-
-
-
-  erb :'/decks/rounds/play'
-end
