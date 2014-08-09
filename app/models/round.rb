28158cc842d5676372d35568abc2f93a74a0b2b9
Joseph White <josephbarrywhite@gmail.com>
2014-08-08 21:18:10 -0400

    fixed next_card and over? methods in round.rb, updated seeds.rb to show answer in question to facilitate testing

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,5 +1,5 @@
 class Round < ActiveRecord::Base
-  # Remember to create a migration!
+
   belongs_to :user
   belongs_to :deck
   has_many :guesses
@@ -8,21 +8,10 @@ class Round < ActiveRecord::Base
   def over?
     @card_list ||= self.deck.cards.map { |card| card.id }
     return true if @card_list.empty?
-    @next_card = Card. findby id = card_list.sample
-    @card_list.delete(@next_card)
-
-    # true unless @played_cards
-  end
-
-# cards = self.deck.cards
-# when guess is made, remove card with card_id from cards
-
-  def played_card
-    Card.find(id: Guess.last.card_id)
   end
 
   def next_card
-    self.deck.cards.sample
+    Card.find(@card_list.shuffle!.pop) unless over?
   end
 
 end
