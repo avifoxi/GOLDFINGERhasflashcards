0755ea46af7789597f10851b4e5d53a4913e93f5
AviFoxi <frosenox@gmail.com>
2014-08-09 22:46:33 -0400

    putting round.over? in round play controller

--- app/controllers/rounds.rb
+++ app/controllers/rounds.rb
@@ -1,33 +1,37 @@
 ### CREATE NEW ROUND
 
-post '/decks/:deck_id/round/create' do
+post '/decks/round/create' do
 
   unless session[:user_id] 
     redirect '/'
   end
 
-  @user = User.find(session[:id])
+  p params
+
+  @user = User.find(session[:user_id])
   @deck = Deck.find(params[:deck_id])
   @round = @user.rounds.create(deck: @deck )
 
-  redirect "/decks/#{@deck.id}/rounds/#{@round.id}/play"
+  redirect "/rounds/#{@round.id}/play"
 end
 
 ## ROUND IN PLAY
 
 get '/rounds/:round_id/play' do
-
-  @round = Round.find(session[:round_id])
+  @round = Round.find(params[:round_id])
+  
+  if @round.over?
+    redirect "/rounds/#{@round.id}/results"
+  end
 
   @card = @round.next_card
-  session[:guess_id] = @guess.id
 
   erb :'/decks/rounds/play'
 end
 
 ## USER'S MOST RECENT ROUND RESULTS
 
-get '/decks/rounds/results' do
+get '/rounds/:round_id/results' do
 
   @round = Round.find(session[:round_id])
   @guesses = Guess.where(round_id: @round.id)
