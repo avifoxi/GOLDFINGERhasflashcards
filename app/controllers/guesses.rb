08fafee6a65411b268f8594b196ff7680ccb576b
AviFoxi <frosenox@gmail.com>
2014-08-09 22:52:34 -0400

    routing

--- app/controllers/guesses.rb
+++ app/controllers/guesses.rb
@@ -1,20 +1,13 @@
-# post 'round/:round_id/card/:card_id/guess/create' do
-#   puts "@guess = Guess.create(params)"
-#   puts "redirect to round/:round_id"
-# end
+## CREATE GUESS WITH INPUT - IF NO INPUT NOT CREATE
+
 
 post '/rounds/:round_id/card/:card_id/guess' do
   @round = Round.find(params[:round_id])
   @card = Card.find(params[:card_id])
 
-  # determine if there is user_input
-  # if no - redir somewhere
-
-  #if yes - create guess
   @guess = @card.guesses.create(params[:guess])
   @round.guesses << @guess
 
   redirect "/rounds/#{@round.id}/play"
-
 
 end
