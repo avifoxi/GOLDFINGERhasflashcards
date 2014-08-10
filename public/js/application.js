dbd157e8ed95617719c7811e98516fec59c70d36
AviFoxi <frosenox@gmail.com>
2014-08-09 23:59:18 -0400

    stuck with modal fade in, done for tonight

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,14 @@
+console.log('hi')
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+	$("#loginButt").click(function(event){
+		event.preventDefault();
+		$(".hide").fadeIn( "slow", function() {
+	    // Animation complete
+	  });
+		// removeClass("hide");
+			// $( ;
+    	
+	});
+
 });
