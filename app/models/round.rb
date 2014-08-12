d0b6b0225c645c75467fc64d79094d17160d0f0e
Joseph White <josephbarrywhite@gmail.com>
2014-08-11 22:55:19 -0400

    final refactoring of round.rb as it was ugly

--- app/models/round.rb
+++ app/models/round.rb
@@ -6,10 +6,10 @@ class Round < ActiveRecord::Base
   has_many :cards, through: :guesses
 
   def over?
-    deck_cards ||= self.deck.cards.map { |card| card.id }
-    played_cards = self.guesses.map { |guess| guess.card_id }
+    deck_cards ||= deck.cards.map { |card| card.id }
+    played_cards = guesses.map { |guess| guess.card_id }
     @card_list = deck_cards - played_cards
-    return true if @card_list.empty?
+    true if @card_list.empty?
   end
 
   def next_card
@@ -17,7 +17,7 @@ class Round < ActiveRecord::Base
   end
 
   def score
-    (guesses.where(correct: true).length * 50 - guesses.where(correct: false).length * 75)
+    guesses.where(:correct => true).count * 50 - guesses.where(:correct => false).count * 75
   end
 
   def self.leaders(num = 3)
